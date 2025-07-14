d3d0b120daecf48f2fb80992100f1857b3e5c980
Andrew21plus <andrew21plus@gmail.com>
2025-07-14 15:30:35 -0500

    Corrige error main

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ app.add_middleware(
 
 app.include_router(auth_router, prefix="/api", tags=["Auth"])
 app.include_router(task_router, prefix="/api", tags=["Tasks"])
+
+# Agrega esto al final del archivo
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
